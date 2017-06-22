8c67d3a20894a2b094fc630702f74e6295cf0520
marko <bokmarko@gmail.com>
2017-06-22 13:47:37 +0200

    hotkey for next and custom buttons, subclassing ActionForm

--- memit/app.py
+++ memit/app.py
@@ -5,10 +5,66 @@ import memit.topic_choice as tc
 from memit.markdown_parser.Section import Section
 
 
-def form_factory(title, text):
-    form = nps.Form(name=title)
-    form.add(nps.Pager, values=text, autowrap=True)
+def form_factory(title, text, callback):
+    # form = MuttPager()
+    # form.wStatus1.value = title
+    # form.wStatus2.value = 'parko'
+    # form.wMain.values = text
+    # form.wMain.autowrap = True
+    # form.add_handlers({
+    #     '^N': callback
+    # })
+
+    form = CustomForm(next_callback=callback, name=title)
+    form.add_widget(nps.Pager, values=text, autowrap=True)
+    form.add_handlers({
+        '^N': callback
+    })
+
     return form
+
+
+class CustomForm(nps.ActionFormV2WithMenus):
+    class next_button(nps.wgbutton.MiniButtonPress):
+        def whenPressed(self):
+            return self.parent._on_next()
+
+    class help_button(nps.wgbutton.MiniButtonPress):
+        def whenPressed(self):
+            return self.parent._on_help()
+
+    def create_control_buttons(self):
+        OFFSET_1 = (1, 6)
+        OFFSET_2 = (1, 30)
+
+        text = 'next (CTRL-n)'
+        self._add_button('next_button',
+                         self.__class__.next_button,
+                         text,
+                         0 - OFFSET_1[0],
+                         0 - OFFSET_1[1] - len(text),
+                         None)
+
+        text = 'help'
+        self._add_button('help_button',
+                         self.__class__.help_button,
+                         text,
+                         0 - OFFSET_2[0],
+                         0 - OFFSET_2[1] - len(text),
+                         None)
+
+    def __init__(self, next_callback, *args, **keywords):
+        super(CustomForm, self).__init__(*args, **keywords)
+        self._on_next = next_callback
+
+    def _on_help(self):
+        pass
+
+
+class MuttPager(nps.FormMuttActive):
+    '''Mutt-style form with a pager widget in the middle
+    '''
+    MAIN_WIDGET_CLASS = nps.Pager
 
 
 class App(nps.NPSAppManaged):
@@ -39,6 +95,11 @@ class App(nps.NPSAppManaged):
         self.tree_choices = self.addForm(
             'topic_choice', tc.Chunk_choice_form, tree)
 
+    def next_form(self):
+        '''switches to next form in line. Will call onInMainLoop by itself
+        '''
+        self.switchFormNow()
+
     def onInMainLoop(self):
         history = self.getHistory()
         last_form = history[len(history) - 1]
@@ -61,7 +122,8 @@ class App(nps.NPSAppManaged):
             self.next_chunk = self.chunks.pop()
             lines = self.next_chunk.get_prompt().split('\n')
             form = form_factory(title=self.next_chunk.get_title(),
-                                text=lines)
+                                text=lines,
+                                callback=self.next_form)
             self.registerForm('show_prompt', form)
         except IndexError:
             self.setNextForm(None)
@@ -70,7 +132,8 @@ class App(nps.NPSAppManaged):
         self.setNextForm('show_answer')
         lines = self.next_chunk.get_content().split('\n')
         form = form_factory(title=self.next_chunk.get_title(),
-                            text=lines)
+                            text=lines,
+                            callback=self.next_form)
         self.registerForm('show_answer', form)
 
 
